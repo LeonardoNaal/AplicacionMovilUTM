9c34498d052c9f58a35760a2f27c8de13a929056
LeonardoNaal <leonardoisraelnaalhernandez@gmail.com>
2017-03-06 16:26:27 -0600

    subi aplicación móvil

---
+++ LinkUTMBetty/app/src/main/java/com/fsociety/linkutmbetty/LogInActivity.java
@@ -0,0 +1,25 @@
+package com.fsociety.linkutmbetty;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        Button btnIniciar = (Button) findViewById(R.id.btnIniciar);
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (LogInActivity.this, UserMainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
